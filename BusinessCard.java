594da490e4877d3990d8460c0d70b1c78fc9aab2
Stephen Cerruti <scerruti@otabi.com>
2024-06-18 10:16:25 -0700

    Create BusinessCard.java

---
+++ BusinessCard.java
@@ -0,0 +1,16 @@
+public class BusinessCard
+{
+	public static void main(String[] args)
+	{
+		//DECLARATION + INITIALIZATION SECTION
+		
+		//INPUT SECTION
+		// N/A
+		
+		//CALCULATION SECTION
+		// N/A
+
+		//OUTPUT SECTION
+		System.out.println();
+	}
+}
